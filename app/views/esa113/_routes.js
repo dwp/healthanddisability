92d8aa4a439881ba339a9b618d2f887fdbcb4b9d
Tom Morgan <tommorgan.digitaldwp@gmail.com>
2017-09-27 13:34:13 +0100

    Mending broken.

---
+++ app/views/esa113/_routes.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var router = express.Router()
+var path = require('path')
+
+router.get('*', function (req, res, next) {
+  // path is only available with the proper value within this sub-module/router.
+  res.locals.path = req.baseUrl.substr(1);
+  next();
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -222,6 +222,7 @@ if (typeof (routes) !== 'function') {
 } else {
   app.use('/assess/v1/', require('./app/views/assess/v1/_routes.js'))
   app.use('/vouch/', require('./app/views/vouch/_routes.js'))
+  app.use('/esa113/', require('./app/views/esa113/_routes.js'))
   app.use('/', routes)
 }
 
