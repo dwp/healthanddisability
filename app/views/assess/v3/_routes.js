e260d8ed5ee7b94e4564d0059011f4434c044ced
Tom Morgan <tommorgan.digitaldwp@gmail.com>
2017-12-08 09:26:42 +0000

    Creating a V3 of assessment tool

---
+++ app/views/assess/v3/_routes.js
@@ -0,0 +1,41 @@
+var express = require('express')
+var router = express.Router()
+var path = require('path')
+var tog = require('../../../../lib/tog.js')
+
+router.get('*', function (req, res, next) {
+  // path is only available with the proper value within this sub-module/router.
+  res.locals.path = req.baseUrl.substr(1)
+  // create some other useful path variables.
+  var bits = req.params[0].substr(1).split('/')
+  res.locals.path1 = res.locals.path + "/" + bits[0]
+  res.locals.path2 = res.locals.path + "/" + bits[0] + "/" + bits[1]
+  res.locals.stage = req.cookies.stage || 1;
+  next()
+})
+
+router.get('/miarussell/*', function (req, res, next) {
+  res.locals.firstname = "Mia"
+  next()
+})
+
+router.get('/cookies/', function (req, res, next) {
+  res.send(tog(req.cookies));
+})
+
+var nug_id = 0;
+
+router.post('/saveText/', function (req, res, next) {
+  var newNugget = {
+    "text":req.body.nugget,
+    "id":nug_id++,
+    "time": new Date()
+  }
+  req.session.data.nuggets = req.session.data.nuggets ? req.session.data.nuggets : [];
+  req.session.data.nuggets.push(newNugget);
+  res.send(tog(req.session.data));
+})
+
+
+
+module.exports = router
