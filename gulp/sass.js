16eb4e673a85a196d8855878fa8d30115536804f
jamie crisp <jamescrisp@digitaldwp.gmail.com>
2018-10-08 14:09:44 +0100

    fixed some capacity stuff and added autoprefeixer

--- gulp/sass.js
+++ gulp/sass.js
@@ -9,6 +9,7 @@ var gulp = require('gulp')
 var sass = require('gulp-sass')
 var sourcemaps = require('gulp-sourcemaps')
 
+const autoprefixer = require('gulp-autoprefixer');
 var config = require('./config.json')
 
 gulp.task('sass', function () {
@@ -18,6 +19,11 @@ gulp.task('sass', function () {
     includePaths: ['govuk_modules/govuk_frontend_toolkit/stylesheets',
       'govuk_modules/govuk_template/assets/stylesheets',
       'govuk_modules/govuk-elements-sass/']}).on('error', sass.logError))
+  .pipe(autoprefixer({
+            browsers: ['last 2 versions', 'ie 6-8'],
+            grid: true,
+            cascade: false
+        }))
   .pipe(sourcemaps.write())
   .pipe(gulp.dest(config.paths.public + '/stylesheets/'))
 })
