44e1e4c4674b807a7e7df0087b591885ee505079
cdenno <chdenno@gmail.com>
2019-01-08 09:40:46 +0000

    v6

---
+++ app/views/fha/v6/data/capacity/monday.js
@@ -0,0 +1,83 @@
+module.exports = [{
+  name: "Manchester",
+  allocatedStaff: [],
+  rooms: 12,
+  morning: {
+    assessments:[],
+    complex: 0,
+    neuro: [],
+    review: []
+    },
+  afternoon: {
+    assessments:[],
+    complex: 0,
+    neuro: [],
+    review: []
+    }
+  },{
+  name: "Burnley",
+  allocatedStaff: [],
+  rooms: 5,
+  morning: {
+    assessments:[],
+    complex: 0,
+    neuro: [],
+    review: []
+    },
+  afternoon: {
+    assessments:[],
+    complex: 0,
+    neuro: [],
+    review: []
+    }
+  },{
+  name: "Bolton",
+  allocatedStaff: [],
+  rooms: 3,
+  morning: {
+    assessments:[],
+    complex: 0,
+    neuro: [],
+    review: []
+    },
+  afternoon: {
+    assessments:[],
+    complex: 0,
+    neuro: [],
+    review: []
+    }
+  },
+  {
+  name: "Wigan",
+  allocatedStaff: [],
+  rooms: 4,
+  morning: {
+    assessments:[],
+    complex: 0,
+    neuro: [],
+    review: []
+    },
+  afternoon: {
+    assessments:[],
+    complex: 0,
+    neuro: [],
+    review: []
+    }
+  },
+  {
+  name: "Stockport",
+  allocatedStaff: [],
+  rooms: 10,
+  morning: {
+    assessments:[],
+    complex: 0,
+    neuro: [],
+    review: []
+  },
+  afternoon: {
+    assessments:[],
+    complex: 0,
+    neuro: [],
+    review: []
+    }
+  }]
