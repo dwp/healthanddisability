62c933cb553236fae86869d5e3b876d09772eb0c
rodmiley <rodmiley5@hotmail.com>
2019-03-26 12:03:23 +0000

    dm assessment updates

---
+++ app/views/fha/v7/caselist/_navItemsDM.js
@@ -0,0 +1,25 @@
+
+module.exports = function(versionNumber, dmCustomers){
+
+
+	return [{
+		url:"decision-maker",
+		label:"Make a decision", 
+		quantity: dmCustomers.length,
+		subItems: [{
+			url: "assessment-reports",
+			label: "Completed assessment",
+			quantity:dmCustomers.filter(customer => customer.status === "Assessment reports").length
+		},{
+			url: "Failed-to-attend",
+			label: "Failure to attend",
+			quantity: dmCustomers.filter(customer => customer.status === "Failed to attend appointment").length
+		}
+	    ,{
+			url: "good-cause",
+			label: "Questionnaire not returned",
+			quantity: dmCustomers.filter(customer => customer.status === "Questionnaire not returned").length
+		}]
+	}
+]
+}
